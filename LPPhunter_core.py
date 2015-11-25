6c3acc4cf6472f343c95d87fce7979b07f6b57a4
Zhixu Ni <zhixu.ni@uni-leipzig.de>
2015-11-25 18:16:27 +0100

    Optimized output fonts and colors, add MS and MS/MS scan time filter

--- LPPhunter_core.py
+++ LPPhunter_core.py
@@ -23,37 +23,38 @@ infile_type = config.get('inputfile', 'filetype')
 ms1_precision = config.get('inputfile', 'ms1_precision')
 msn_precision = config.get('inputfile', 'msn_precision')
 
-rt_range = [24.0, 30.0]
+rt_range = [10.0, 30.0]
 
-mz2get = 778.560
+# mz2get = 778.560
+mz2get_lst = [778.560, 776.544, 774.529, 806.591]
+for mz2get in mz2get_lst:
+    if infile_type.lower() == 'mzml':
+        infile_name = config.get('inputfile', 'filename')
+        print infile_name
+        encode_typ = check_encode(infile_name)
+        print 'assume to be:', encode_typ, 'encoded'
 
-if infile_type.lower() == 'mzml':
-    infile_name = config.get('inputfile', 'filename')
-    print infile_name
-    encode_typ = check_encode(infile_name)
-    print 'assume to be:', encode_typ, 'encoded'
+        xic_spec = XIC(infile_name, encode_typ, ms1_precision, msn_precision)
 
-    xic_spec = XIC(infile_name, encode_typ, ms1_precision, msn_precision)
+        rt_lst, xic_df, ms_spectra_dct = xic_spec.extract_mz(mz2get, rt_range)
 
-    rt_lst, xic_df, ms_spectra_dct = xic_spec.extract_mz(mz2get, rt_range)
+        print 'main peaks in range', rt_lst
+        print xic_df.tail(5)
+        msms_spec = MSMS(infile_name, encode_typ, ms1_precision, msn_precision)
+        msms_spectra_dct = msms_spec.get_ms2(mz2get, rt_lst)
 
-    print 'main peaks in range', rt_lst
-    print xic_df.tail(5)
-    msms_spec = MSMS(infile_name, encode_typ, ms1_precision, msn_precision)
-    msms_spectra_dct = msms_spec.get_ms2(mz2get, rt_lst)
+        spec_plt = Spectra_Ploter()
+        spec_plt.plot_all(mz2get, xic_df, ms_spectra_dct, msms_spectra_dct)
 
-    spec_plt = Spectra_Ploter()
-    spec_plt.plot_all(mz2get, xic_df, ms_spectra_dct, msms_spectra_dct)
+    # if infile_type.lower() == 'mzml.gz':
+    #     infile_name = config.get('inputfile', 'filename')
+    #     print infile_name
+    #
+    #     xic_spec = XIC(infile_name)
+    #     xic_spec.extract_mz(778.560, ppm=20)
 
-# if infile_type.lower() == 'mzml.gz':
-#     infile_name = config.get('inputfile', 'filename')
-#     print infile_name
-#
-#     xic_spec = XIC(infile_name)
-#     xic_spec.extract_mz(778.560, ppm=20)
-
-else:
-    print 'No input mzML'
+    else:
+        print 'No input mzML'
 
 ed_time = time.clock() - st_time
 print ed_time
